8043ded7e9f9dd8aa03e5cf2a0b826bfc1dc794a
Md. Asik Islam <115912089+asikislam787878@users.noreply.github.com>
2022-11-07 21:43:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// Navbar scrolling  set................................
+function changBg(){
+    var navbar = document.getElementById('navbar');
+    var scrollValue = window.scrollY;
+        navbar.classList.add('navbarBgC');
+}
+window.addEventListener('scroll', changBg);
+
+
+
+
+
+var typed2 = new Typed('.animation_mydetell', {
+    strings: ['Web Developer', 'Web Deginers', 'Graphis Deginers', 'Photoshop expert'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    // fadeOut: true,
+    loop: true
+});
+
+
+/* <!----------------------------------- Category Footer pages  start ----------------------------> */
+
+/* <!----------------------------------- Category Footer pages  end ----------------------------> */
+
